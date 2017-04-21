34dfc1dc93dc7f4840cf9fb7900e8debc796be6a
Halbert Stone <halbertstone@gmail.com>
2017-04-21 04:20:05 +0000

    Parent child and super

--- Raspin.py
+++ Raspin.py
@@ -3,27 +3,92 @@
 #  2017-04-16
 #  Class to represent a single Item connection on the rasp pi a single GPIO 
 #
-"""
-Turn a item on via the Raspberry Pi GPIO 
-
-"""
 
 import RPi.GPIO as GPIO
 import time
 
 
 class Raspin():
-    ioid = None
-    state = True
+    """
+    Utility for working with the raspberry pi GPIO PINS
+    based on the GPIO.BCM pin numbering scheme
+    Concept: Peripheral is connected to a GPIO PIN
+    Class can be used to associate the peripheral with 
+    assigned PIN  and manipulate PIN power on, off, toggle
 
-    def __init__(self, ioid, state=False):
+    USAGE: 
+     ioid = None
+     pin_map = None
+    """
+
+    def __init__(self
+            , ioid
+            , pin_map=GPIO.BCM):
+        """ param: ioid is the pin number in the selected pin_mode
+        param: pin_mode is the pin numbering system selected
+        param: state is the initial power setting for the pin
+        """
         self.ioid = ioid
-        self.state = state
+        self.pin_mode = pin_mode
 
-    def setup(self, state):
+    def setup_output(self, state):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.ioid, GPIO.OUT)
+	
+    def on(self):
+        self.state = True
         GPIO.output(self.ioid, self.state)
+	
+    def off(self ):
+        self.state = False
+        GPIO.output(self.ioid, self.state)
+	
+    def switch(self):
+        self.state = not self.state
+        GPIO.output(self.ioid, self.state)
+
+	
+class PinOut(Raspin):
+    """
+    Utility for working with the raspberry pi GPIO PINS
+    based on the GPIO.BCM pin numbering scheme
+    Concept: Peripheral is connected to a GPIO PIN
+    Class can be used to associate the peripheral with 
+        assigned PIN  and manipulate PIN power on, off, toggle
+    
+    USAGE: 
+        led17 = PinOut(17)
+        led17.on() for power on
+        led17.off() for power off
+        led17.switch() to toggle power
+         param: ioid is the pin number in the selected pin_mode
+         param: pin_mode is GPIO.OUT (default)
+         param: pin_map is pin number scheme GPIO.BCM (defalut), or GPIO.BOARD
+         param: state is the initial power setting for the pin False (default)
+        
+    """
+    state = False
+    pin_mode = None
+
+    def __init__(self
+            , ioid
+            , pin_mode=GPIO.OUT
+            , pin_map=GPIO.BCM
+            , state=False):
+        # TODO: if not ioid in range(42): define valid numbers per pin_map
+        # TODO:     raise exception   define appropriate exception
+        # TODO: if pin_mode in ["GPIO.BCM", "GPIO.BOARD"]:
+        
+        super(type(PinOut), self).__init__(ioid, pin_map)
+
+        # TODO: if state in [True, False]:
+        self.state = state
+        self.setup_output()
+
+
+    def setup_output(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.ioid, GPIO.OUT)
 	
     def on(self):
         self.state = True
@@ -38,9 +103,9 @@ class Raspin():
         GPIO.output(self.ioid, self.state)
 	
 def main():
-    led17 = Raspin(17,True)
+    led17 = PinOut(17)
+    led17.setup()
 
-    led17.setup(True)
     print("led17 setup and now: led17.on()")
     led17.on()
     print("time.sleep 2")
